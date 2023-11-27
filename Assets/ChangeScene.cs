5685a03b8ae31796326da13049b123fe4bedd961
prajnavohraa <prajna21345@iiitd.ac.in>
2023-11-27 21:19:36 +0530

    new earth scene

---
+++ Assets/ChangeScene.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // public string scenename;
+    void OnTriggerEnter(Collider other){
+        // if(other.CompareTag("Player")){
+        //     SceneManager.LoadScene(scenename);
+        // }
+        SceneManager.LoadScene("MoonScene");
+    }
+}
